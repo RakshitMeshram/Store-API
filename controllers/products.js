ea7bd707d3de9aa70e50346c401c3de735878f0b
Rakshit Meshram <rakshitmeshram10@gmail.com>
2022-07-31 20:09:49 +0530

    Filter Functionality

---
+++ controllers/products.js
@@ -0,0 +1,13 @@
+const Product = require('../models/Product')
+
+const getAllProductsStatic = async(req,res) => {
+    const products = await Product.find({featured:true})
+    res.status(200).json({ products, nbHits: products.length })
+}
+
+const getAllProducts = async(req,res) => {
+    res.status(200).json({ msg: 'products route'})
+}
+
+
+module.exports = { getAllProductsStatic, getAllProducts}
